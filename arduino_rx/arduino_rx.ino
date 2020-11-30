cac88d533e9439a208a1161edc95669f8f58426e
Dr. Rajesh Singh <75157838+RajeshSingh001@users.noreply.github.com>
2020-11-30 11:13:38 +0530

    RX/TX code and Proteus model

---
+++ arduino_rx/arduino_rx.ino
@@ -0,0 +1,38 @@
+/* RECIEVER */
+
+#include <LiquidCrystal.h>
+
+LiquidCrystal lcd(12,11,6,5,4,3);
+
+void setup()
+{
+  Serial.begin(9600);
+  lcd.begin(32,2);
+}
+
+void loop()
+{
+  lcd.clear();
+  lcd.setCursor(0,0);
+  if(Serial.read()==HIGH)
+  {
+    lcd.print("Digital Sensor ON");
+    lcd.setCursor(0,1);
+    lcd.print("recieving TEMP-SENSOR data....");
+    Serial.print("temp : ");
+    Serial.print(Serial.read());
+    Serial.println("   *C");
+    delay(500);
+  }
+  else
+  {
+    lcd.print("Digital Sensor OFF");
+    lcd.setCursor(0,1);
+    lcd.print("recieving TEMP-SENSOR data....");
+    Serial.print("temp : ");
+    Serial.print(Serial.read());
+    Serial.println("   *C");
+  }
+  
+  delay(50);
+}
---
+++ arduino_tx/arduino_tx.ino
@@ -0,0 +1,41 @@
+/* TRANSMITTER */
+
+#include <LiquidCrystal.h>
+
+LiquidCrystal lcd(12,11,6,5,4,3);
+int TEMP_SEN=A0;
+
+void setup()
+{
+  pinMode(7,INPUT_PULLUP);
+  Serial.begin(9600);
+  lcd.begin(32,2);
+}
+
+void loop()
+{
+  int digitalStatus=0;
+  lcd.clear();
+  lcd.setCursor(0,0);
+  if(digitalRead(7)==LOW)
+  {
+    digitalStatus=1;
+    lcd.print("Digital Sensor ON");
+    lcd.setCursor(0,1);
+    lcd.print("sending TEMP-SENSOR data....");
+    Serial.write(digitalStatus);
+    Serial.write(analogRead(TEMP_SEN)/2-1);
+    delay(500);
+  }
+  else if(digitalRead(7)==HIGH)
+  {
+    digitalStatus=0;
+    lcd.print("Digital Sensor OFF");
+    lcd.setCursor(0,1);
+    lcd.print("sending TEMP-SENSOR data....");
+    Serial.write(digitalStatus);
+    Serial.write(analogRead(TEMP_SEN)/2-1);
+  }
+  
+  delay(50);
+}
